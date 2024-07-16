2047b15b76f9379c69f8ef4058e2325198ee157e
HDash <16350928+HDash@users.noreply.github.com>
2024-07-16 11:27:28 +0100

    Add reports for CTCF_ChIP-TIP and built_in_data

---
+++ CTCF_ChIP-TIP/run.R
@@ -0,0 +1,34 @@
+library(MotifPeeker)
+
+# Load the data
+alignments <- c("./CTCF_ChIP-TIP/input/1A_CTCF_TIPSeq_alignment_firstmate.bam",
+                "./CTCF_ChIP-TIP/input/1A_CTCF_ChIPSeq_alignment_ENCFF172XLC.bam",
+                "./CTCF_ChIP-TIP/input/1B_CTCF_ChIPSeq_alignment_ENCFF411OOS.bam",
+                "./CTCF_ChIP-TIP/input/1C_CTCF_ChIPSeq_alignment_ENCFF091ODJ.bam")
+
+peaks <- c("./CTCF_ChIP-TIP/input/1A_CTCF_TIPSeq_alignment_firstmate_peaks_qvalue0.01.narrowPeak",
+           "./CTCF_ChIP-TIP/input/1A_CTCF_ChIPSeq_alignment_ENCFF172XLC_peaks_qvalue0.01.narrowPeak",
+           "./CTCF_ChIP-TIP/input/1B_CTCF_ChIPSeq_alignment_ENCFF411OOS_peaks_qvalue0.01.narrowPeak",
+           "./CTCF_ChIP-TIP/input/1C_CTCF_ChIPSeq_alignment_ENCFF091ODJ_peaks_qvalue0.01.narrowPeak")
+
+motifs <- c("./CTCF_ChIP-TIP/input/1A_motif_MA1930.2.jaspar",
+            "./CTCF_ChIP-TIP/input/1B_motif_MA1102.3.jaspar")
+
+result <- MotifPeeker::MotifPeeker(
+    peak_files = peaks,
+    reference_index = 1,
+    alignment_files = alignments,
+    exp_labels = c("TIPSeq", "ChIPSeq1", "ChIPSeq2", "ChIPSeq3"),
+    exp_type = c("tipseq", "chipseq", "chipseq", "chipseq"),
+    genome_build = "hg38",
+    motif_files = motifs,
+    motif_labels = c("MA1930.2", "MA1102.3"),
+    denovo_motifs = 5,
+    out_dir = "./CTCF_ChIP-TIP/output",
+    save_runfiles = TRUE,
+    workers = 2,
+    quiet = FALSE,
+    debug = TRUE,
+    verbose = TRUE
+)
+
---
+++ built_in_data/run.R
@@ -0,0 +1,38 @@
+library(MotifPeeker)
+
+# Load the data
+data("CTCF_ChIP_peaks", package = "MotifPeeker")
+data("CTCF_TIP_peaks", package = "MotifPeeker")
+data("motif_MA1102.3", package = "MotifPeeker")
+data("motif_MA1930.2", package = "MotifPeeker")
+CTCF_ChIP_alignment <- system.file("extdata", "CTCF_ChIP_alignment.bam",
+                                   package = "MotifPeeker")
+CTCF_TIP_alignment <- system.file("extdata", "CTCF_TIP_alignment.bam",
+                                  package = "MotifPeeker")
+
+# Prepare the data
+peak_files <- list(CTCF_ChIP_peaks, CTCF_TIP_peaks)
+alignment_files <- list(CTCF_ChIP_alignment, CTCF_TIP_alignment)
+motif_files <- list(motif_MA1102.3, motif_MA1930.2)
+
+# Run MotifPeeker
+result <- MotifPeeker(
+    peak_files = peak_files,
+    reference_index = 2,  # Set TIP-seq experiment as reference
+    alignment_files = alignment_files,
+    exp_labels = c("ChIP", "TIP"),
+    exp_type = c("chipseq", "tipseq"),
+    genome_build = "hg38",  # Use hg38 genome build
+    motif_files = motif_files,
+    cell_counts = NULL,  # No cell-count information
+    denovo_motif_discovery = TRUE,
+    denovo_motifs = 3,  # Discover top 3 motifs
+    motif_db = NULL,  # Use default motif database (JASPAR)
+    download_buttons = TRUE,
+    out_dir = "./built_in_data/output",
+    save_runfiles = TRUE,
+    workers = 2,  # Use two CPU cores on a 16GB RAM machine
+    debug = FALSE,
+    quiet = TRUE,
+    verbose = TRUE
+)
